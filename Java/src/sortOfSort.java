9276e7eec22f5fbc1388fc8a2a3b0dc049776054
jrubio15 <60449015+jrubio15@users.noreply.github.com>
2020-03-07 01:35:25 -0700

    Update sortOfSort.java

--- Java/src/sortOfSort.java
+++ Java/src/sortOfSort.java
@@ -1,23 +1,23 @@
 // Javier Rubio
 public class sortOfSort {
     public static void sortOfSort(int[] nums) {
-        //The smallest variable
+        
         int lowIndex= 0;
-        //The highest variable
+        
         int highIndex = nums.length-1;
-        //This variable hold the highest value
+        
         int maximum = Integer.MIN_VALUE;
-        //Index of the current highest
+        
         int indexCurrent = 0;
-        //the variable's position
+        
         int count = 0;
-        //swapping the variables
+     
         int swap = 0;
 
         for(int i = 0; i < nums.length-1; i++){
-            //Resetting the max
+            
             maximum = Integer.MIN_VALUE;
-            //Updating
+            
             indexCurrent = i;
 
             //Finding the max variable
